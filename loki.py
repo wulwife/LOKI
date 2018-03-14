ffbb7c6488b702b3454c430c24584e1f92d44fde
Francesco <fsco.grigoli@gmail.com>
2018-03-14 18:36:47 +0100

    added loki class

--- loki.py
+++ loki.py
@@ -1,144 +1,187 @@
+import numpy as num
 import traveltimes
 import waveforms
-from datetime import datetime
-import numpy as num
-import matplotlib.pyplot as plt
-import DET_STALTA
-import LOC_STALTA
+import stacktraces
 
 class Loki:
+    """docstring for Loki"""
 
-    def __init__(self, tobj, wobj, derivative=True):
-        #check input objects
-        self.check_sampling_rate(wobj)
-        self.check_starting_time(wobj)
-        self.loki_input(wobj, tobj, derivative)
-
-    def check_sampling_rate(self,wobj):
-        intsamp=1E6
-        deltas=[]
-        for comp in (wobj.stream).keys():
-            for sta in (wobj.stream[comp]).keys():
-                deltas.append(wobj.stream[comp][sta][1])
-        deltas=num.array(deltas)
-        ideltas=num.unique((deltas*intsamp).astype(int))
-        if num.size(ideltas)==1:
-           self.deltat=deltas[0]
+    def __init__(self, data_path, output_path, db_path, hdr_filename, mode='locator'):
+        self.data_path=data_path
+        self.output_path=output_path
+        self.db_path=db_path
+        self.hdr_filename=hdr_filename
+        if mode=='locator':
+            self.data_tree, self.events=self.location_data_struct(self.data_path, self.output_path)
+        elif mode=='detector':
+            self.data_tree, self.events=self.detection_data_struct(self.data_path, self.output_path)
         else:
-           raise ValueError('Error!! All trace must have the same sampling rate')
+            raise ValueError('mode must be "detector" or "locator"')
 
-    def check_starting_time(self,wobj):
-        intsamp=1E6
-        dtimes=[]
-        self.ns=0
-        for comp in (wobj.stream).keys():
-            for sta in (wobj.stream[comp]).keys():
-                dtimes.append(wobj.stream[comp][sta][0])
-                if self.ns<num.size(wobj.stream[comp][sta][2]):
-                   self.ns=num.size(wobj.stream[comp][sta][2])
-        self.dtime_max=max(dtimes)
-        self.evid=(self.dtime_max).isoformat()
 
-    def loki_input(self, wobj, tobj, derivative):
-        self.comp=tuple((wobj.stream).keys())
-        if len(self.comp)==3:
-            self.xtr=self.select_data(self.comp[0], wobj, tobj.db_stations, derivative)
-            self.ytr=self.select_data(self.comp[1], wobj, tobj.db_stations, derivative)
-            self.ztr=self.select_data(self.comp[2], wobj, tobj.db_stations, derivative)
-        elif len(self.comp)==1:
-            self.ztr=self.select_data(comp, wobj.data_stations, db_stations, derivative)
-        else:
-            raise ValueError('Traces must have 1 or 3 components!')
+# encapsulate data_struct in a class data_tree
+    def location_data_struct(self, data_path, output_path):
+        events=[]
+        data_tree=[]
+        for root,dirs,files in os.walk(data_path):
+           if not dirs:
+              data_tree.append(root)
+              events.append(root.split('/')[-1])
+        for event in events:
+           if not os.path.isdir(output_path):
+              os.mkdir(output_path)
+        return data_tree, events
 
-    def select_data(self, comp, wobj, db_stations, derivative):
-        self.stations=tuple(wobj.data_stations & db_stations)
-        self.nstation=num.size(self.stations)
-        tr=num.zeros([self.nstation,self.ns])
-        stream=wobj.stream[comp]
-        for i,sta in enumerate(self.stations):
-            nstr=num.size(stream[sta][2])
-            idt=num.int((self.dtime_max-stream[sta][0]).total_seconds()/self.deltat)
-            tr[i,0:nstr-idt]=stream[sta][2][idt:]
-            if derivative:
-               tr[i,1:self.ns]=((tr[i,1:]-tr[i,0:self.ns-1])/self.deltat)
-               tr[i,0]=0.
+    def detection_data_struct(self, data_path, output_path):
+        events=[]
+        data_tree=[]
+        for root,dirs,files in os.walk(data_path):
+           if not dirs:
+              data_tree.append(root)
+              events.append(root.split('/')[-1])
+        for event in events:
+           if not os.path.isdir(output_path):
+              os.mkdir(output_path)
+        return data_tree, events
+#############
+
+    def location(self, extension='*', comp=['E','N','Z'], precision='single', **inputs):
+        tshortp_min=inputs['tshortp_min']; tshortp_max=inputs['tshortp_max'];
+        tshorts_min=inputs['tshorts_min']; tshortp_max=inputs['tshorts_max'];
+        slrat=inputs['slrat']
+        npr=inputs['npr']
+        ntrial=inputs['ntrial']
+        derivative=inputs['derivative']
+        tshortp=num.linspace(tshortp_min,tshortp_max,ntrial)
+        tshorts=num.linspace(tshorts_min,tshorts_max,ntrial)
+
+
+        tobj=traveltimes(self.db_path, self.hdr_filename)
+        tp=tobj.load_traveltimes('P', precision)
+        ts=tobj.load_traveltimes('S', precision)
+
+
+        for event_path in self.data_tree:
+
+            wobj=waveforms(event_path, extension, comp)
+            sobj=stacktraces(tobj, wobj, derivative=True)
+            event=sobj.evid
+
+
+            print('accessing to the event folder: ', event_path, event)
+            if os.path.isdir(self.output_path+'/'+event):
+                continue
             else:
-               tr[i,:]=1.
-        return tr
+                os.mkdir(self.output_path+'/'+event)
 
-    def time_extractor(self, tp, ts):
-        nxyz= num.size(tp[self.stations[0]])
-        tp_mod=num.zeros([nxyz,self.nstation]) #optimize with pointers to tp[sta] and ts[sta]
-        ts_mod=num.zeros([nxyz,self.nstation])
-        for i,sta in enumerate(self.stations):
-            tp_mod[:,i]=tp[sta]
-            ts_mod[:,i]=ts[sta]
-        return (tp_mod, ts_mod)
 
-    def cfunc_erg(self, ergz=True):
-        if ergz:
-           self.obs_dataV=(self.ztr**2)
+            tp_mod, ts_mod=sobj.time_extractor(tp, ts)
+            tp_mod, ts_mod=tt_processing.tt_f2i(loc.deltat,tp_mod,ts_mod, npr)
+
+
+            for i in range(ntrial):
+                nshort_p=int(tshortp[i]//sobj.deltat)
+                nshort_s=int(tshorts[i]//sobj.deltat)
+                nlong_s=int(nshort_p*slrat)
+                nlong_s=int(nshort_s*slrat)
+                obs_dataP, obs_dataS=sobj.loc_stalta(nshort_p, nshort_s, slrat, thres=2)
+                corrmatrix=location.stacking(tp_mod, ts_mod, obs_dataP, obs_dataS, npr)
+                cmax=num.max(corrmatrix)
+                corrmatrix=num.reshape(corrmatrix,(traveldb.nx,traveldb.ny,traveldb.nz))
+                (ixloc,iyloc,izloc)=num.unravel_index(num.argmax(corrmatrix),(traveldb.nx,traveldb.ny,traveldb.nz))
+                #xloc=ixloc*traveldb.dx; yloc=iyloc*traveldb.dy; zloc=izloc*traveldb.dz
+                xloc=traveldb.x[ixloc]; yloc=traveldb.y[iyloc]; zloc=traveldb.x[izloc]
+                out_file = open(self.output_path+'/'+event+'/'+event+'.loc','a')
+                out_file.write(str(i)+' '+str(xloc)+' '+str(yloc)+' '+str(zloc)+' '+str(cmax)+' '+str(nshort)+' '+str(nlong)+'\n')
+                out_file.close()
+                num.save(self.output_path+'/'+event+'/'+'corrmatrix_trial_'+str(ntrial),corrmatrix)
+                self.coherence_plot(self.output_path+'/'+event, corrmatrix, traveldb.x, traveldb.y, traveldb.z, i)
+            self.catalogue_creation(event, traveldb.lat0, traveldb.lon0, ntrial)
+        print('Ho finito!!!')
+
+
+    def catalogue_creation(self, event, lat0, lon0, ntrial, refell=23):
+        (zorig,eorig,norig)=LatLongUTMconversion.LLtoUTM(refell, lat0, lon0) #da adeguare in python 3
+        ev_file=self.output_path+'/'+event+'/'+event+'.loc'
+        data=num.loadtxt(ev_file)
+        if (ntrial>1):
+           w=num.sum(data[:,4])
+           xb= ((num.dot(data[:,1],data[:,4])/w)*1000)+eorig
+           yb= ((num.dot(data[:,2],data[:,4])/w)*1000)+norig
+           late,lone=LatLongUTMconversion.UTMtoLL(refell, yb, xb, zorig)
+           zb= num.dot(data[:,3],data[:,4])/w
+           cb= num.mean(data[:,4])
+           cmax=num.max(data[:,4])
+           merr=num.vstack((data[:,1],data[:,2],data[:,3]))
+           err=num.cov(merr)
+           errmax= num.sqrt(num.max(num.linalg.eigvals(err)))
         else:
-           self.obs_dataV=(self.ztr**2)
-           self.obs_dataH=(self.xtr**2)*(self.ytr**2)
-
-    def cfunc_pcafull(self, epsilon=0.001):
-        obs_dataH=num.zeros([self.nstation,self.ns]); obs_dataV=num.zeros([self.nstation,self.ns])
-        obs_dataH1=hilbert(self.xtr); obs_dataH2=hilbert(self.ytr); obs_dataH3=hilbert(self.ztr)
-        obs_dataH1C=num.conjugate(obs_dataH1); obs_dataH2C=num.conjugate(obs_dataH2); obs_dataH3C=num.conjugate(obs_dataH3)
-        xx=obs_dataH1*obs_dataH1C; xy=obs_dataH1*obs_dataH2C; xz=obs_dataH1*obs_dataH3C
-        yx=obs_dataH2*obs_dataH1C; yy=obs_dataH2*obs_dataH2C; yz=obs_dataH2*obs_dataH2C
-        zx=obs_dataH3*obs_dataH1C; zy=obs_dataH3*obs_dataH2C; zz=obs_dataH3*obs_dataH3C
-        for i in range(self.nstation):
-            for j in range(self.ns):
-                cov3d=num.array([[xx[i,j], xy[i,j], xz[i,j]],[yx[i,j], yy[i,j], yz[i,j]],[zx[i,j],zy[i,j],zz[i,j]]])
-                cov2d=num.array([[xx[i,j], xy[i,j]],[yx[i,j], yy[i,j]]])
-                U2d, s2d, V2d = num.linalg.svd(cov2d, full_matrices=True)
-                U3d, s3d, V3d = num.linalg.svd(cov3d, full_matrices=True)
-                obs_dataV[i,j]=(s3d[0]**2)
-                obs_dataH[i,j]=(s2d[0]**2)
-            obs_dataH[i,:]=(obs_dataH[i,:]/num.max(obs_dataH[i,:]))+epsilon
-            obs_dataV[i,:]=(obs_dataV[i,:]/num.max(obs_dataV[i,:]))
-        self.obs_dataH=obs_dataH
-        self.obs_dataV=obs_dataV
-
-    def cfunc_pca(self, epsilon=0.001):
-        obs_dataH=num.zeros([self.nstation,self.ns])
-        obs_dataH1=hilbert(self.xtr)
-        obs_dataH2=hilbert(self.ytr)
-        obs_dataH1C=num.conjugate(obs_dataH1)
-        obs_dataH2C=num.conjugate(obs_dataH2)
-        xx=obs_dataH1*obs_dataH1C; xy=obs_dataH1*obs_dataH2C
-        yx=obs_dataH2*obs_dataH1C; yy=obs_dataH2*obs_dataH2C
-        for i in range(self.nstation):
-            for j in range(self.ns):
-                cov=num.array([[xx[i,j], xy[i,j]],[yx[i,j], yy[i,j]]])
-                U, s, V = num.linalg.svd(cov, full_matrices=True)
-                obs_dataH[i,j]=(s[0]**2)
-            obs_dataH[i,:]=(obs_dataH[i,:]/num.max(obs_dataH[i,:]))+epsilon
-        self.obs_dataH=obs_dataH
+           late,lone=LatLongUTMconversion.UTMtoLL(refell, (data[2]*1000)+norig, (data[1]*1000)+eorig, zorig)
+           zb= data[3]; errmax='NA'; cb=data[4]; cmax=data[4];
+        f=open(self.output_path+'/'+'catalogue','a')
+        f.write(event+'    '+str(late)+'   '+str(lone)+'   '+str(zb)+'   '+str(errmax)+'   '+str(cb)+'   '+str(cmax)+'\n')
+        f.close()
 
 
-#locations class?
-    def loc_stalta(self, nshort_p, nshort_s, slrat, thres=2):
-        tshort_p=nshort_p*self.deltat; tshort_s=nshort_s*self.deltat
-        tlong_p=tshort_p*slrat; tlong_s=tshort_s*slrat
-        ks_p=self.deltat/tshort_p; kl_p=self.deltat/tlong_p;
-        ks_s=self.deltat/tshort_s; kl_s=self.deltat/tlong_s;
-        obs_dataP=LOC_STALTA.recursive_stalta(tshort_p, tlong_p, self.deltat, self.obs_dataV, kl_p, ks_p, thres)
-        obs_dataS=LOC_STALTA.recursive_stalta(tshort_s, tlong_s, self.deltat, self.obs_dataH, kl_s, ks_s, thres)
-        return obs_dataP, obs_dataS
+
+    def coherence_plot(self, event_path, corrmatrix, xax, yax, zax, ntrial, normalization=False):
+        nx,ny,nz=num.shape(corrmatrix)
+        CXY=num.zeros([ny, nx])
+        for i in range(ny):
+            for j in range(nx):
+                CXY[i,j]=num.max(corrmatrix[j,i,:])
+
+        CXZ=num.zeros([nz, nx])
+        for i in range(nz):
+            for j in range(nx):
+			             CXZ[i,j]=num.max(corrmatrix[j,:,i])
 
 
-#detection class?
-    def det_stalta(self, nshort_p, nshort_s, slrat, staltap0, staltas0, thres=0.):
-        tshort_p=nshort_p*self.deltat; tshort_s=nshort_s*self.deltat
-        tlong_p=tshort_p*slrat; tlong_s=tshort_s*slrat
-        obs_dataP=num.zeros([self.nstation,self.ns])
-        obs_dataS=num.zeros([self.nstation,self.ns])
-        for i in range(self.nstation):
-            obs_dataP[i,:],stltp0=DET_STALTA.recursive_stalta(staltap0[i][0], staltap0[i][1], tshort_p, tlong_p, self.deltat, self.obs_dataV[i,:], thres)
-            obs_dataS[i,:],stlts0=DET_STALTA.recursive_stalta(staltas0[i][0], staltas0[i][1], tshort_s, tlong_s, self.deltat, self.obs_dataH[i,:], thres)
-            staltap0[i][0]=stltp0[0]; staltap0[i][1]=stltp0[1]
-            staltas0[i][0]=stlts0[0]; staltas0[i][1]=stlts0[1]
-        return obs_dataP, obs_dataS, staltap0, staltas0
+        CYZ=num.zeros([nz, ny])
+        for i in range(nz):
+            for j in range(ny):
+                CYZ[i,j]=num.max(corrmatrix[:,j,i])
+
+        if normalization:
+           nrm=Normalize(vmin=0., vmax=1.)
+        else:
+           nrm=None
+
+
+        fig = plt.figure()
+        fig.suptitle('Coherence matrix X-Y', fontsize=14, fontweight='bold')
+        ax = fig.gca()
+        cmap = plt.cm.get_cmap('jet', 100)
+        cs = plt.contourf(xax, yax, CXY, 20, cmap=cmap, interpolation='bilinear', norm=nrm)
+        ax.set_xlabel('X (km)')
+        ax.set_ylabel('Y (km)')
+        cbar = plt.colorbar(cs)
+        plt.axes().set_aspect('equal')
+        plt.savefig(event_path+'/'+'Coherence_matrix_xy'+str(ntrial)+'.eps')
+
+
+        fig = plt.figure()
+        fig.suptitle('Coherence matrix X-Z', fontsize=14, fontweight='bold')
+        ax = fig.gca()
+        cmap = plt.cm.get_cmap('jet', 100)
+        cs = plt.contourf(xax, zax, CXZ, 20, cmap=cmap, interpolation='bilinear', norm=nrm)
+        ax.set_xlabel('X (km)')
+        ax.set_ylabel('Z (km)')
+        cbar = plt.colorbar(cs)
+        ax.invert_yaxis()
+        plt.axes().set_aspect('equal')
+        plt.savefig(event_path+'/'+'Coherence_matrix_xz'+str(ntrial)+'.eps')
+
+
+        fig = plt.figure()
+        fig.suptitle('Coherence matrix Y-Z', fontsize=14, fontweight='bold')
+        ax = fig.gca()
+        cmap = plt.cm.get_cmap('jet', 100)
+        cs = plt.contourf(yax, zax, CYZ, 20, cmap=cmap, interpolation='bilinear', norm=nrm)
+        ax.set_xlabel('Y(km)')
+        ax.set_ylabel('Z (km)')
+        ax.invert_yaxis()
+        cbar = plt.colorbar(cs)
+        plt.axes().set_aspect('equal')
+        plt.savefig(event_path+'/'+'Coherence_matrix_yz'+str(ntrial)+'.eps')
+        plt.close("all")
---
+++ stacktraces.py
@@ -0,0 +1,140 @@
+import traveltimes
+import waveforms
+from datetime import datetime
+import numpy as num
+import matplotlib.pyplot as plt
+import DET_STALTA
+import LOC_STALTA
+
+class Stacktraces:
+
+    def __init__(self, tobj, wobj, derivative=True):
+        #check input objects tobj=traveltime_object wobj=Raw_waveform_object
+        self.check_sampling_rate(wobj)
+        self.check_starting_time(wobj)
+        self.loki_input(wobj, tobj, derivative)
+
+    def check_sampling_rate(self,wobj):
+        intsamp=1E6
+        deltas=[]
+        for comp in (wobj.stream).keys():
+            for sta in (wobj.stream[comp]).keys():
+                deltas.append(wobj.stream[comp][sta][1])
+        deltas=num.array(deltas)
+        ideltas=num.unique((deltas*intsamp).astype(int))
+        if num.size(ideltas)==1:
+           self.deltat=deltas[0]
+        else:
+           raise ValueError('Error!! All trace must have the same sampling rate')
+
+    def check_starting_time(self,wobj):
+        intsamp=1E6
+        dtimes=[]
+        self.ns=0
+        for comp in (wobj.stream).keys():
+            for sta in (wobj.stream[comp]).keys():
+                dtimes.append(wobj.stream[comp][sta][0])
+                if self.ns<num.size(wobj.stream[comp][sta][2]):
+                   self.ns=num.size(wobj.stream[comp][sta][2])
+        self.dtime_max=max(dtimes)
+        self.evid=(self.dtime_max).isoformat()
+
+    def loki_input(self, wobj, tobj, derivative):
+        self.comp=tuple((wobj.stream).keys())
+        if len(self.comp)==3:
+            self.xtr=self.select_data(self.comp[0], wobj, tobj.db_stations, derivative)
+            self.ytr=self.select_data(self.comp[1], wobj, tobj.db_stations, derivative)
+            self.ztr=self.select_data(self.comp[2], wobj, tobj.db_stations, derivative)
+        elif len(self.comp)==1:
+            self.ztr=self.select_data(comp, wobj.data_stations, db_stations, derivative)
+        else:
+            raise ValueError('Traces must have 1 or 3 components!')
+
+    def select_data(self, comp, wobj, db_stations, derivative):
+        self.stations=tuple(wobj.data_stations & db_stations)
+        self.nstation=num.size(self.stations)
+        tr=num.zeros([self.nstation,self.ns])
+        stream=wobj.stream[comp]
+        for i,sta in enumerate(self.stations):
+            nstr=num.size(stream[sta][2])
+            idt=num.int((self.dtime_max-stream[sta][0]).total_seconds()/self.deltat)
+            tr[i,0:nstr-idt]=stream[sta][2][idt:]
+            if derivative:
+               tr[i,1:self.ns]=((tr[i,1:]-tr[i,0:self.ns-1])/self.deltat)
+               tr[i,0]=0.
+            else:
+               tr[i,:]=1.
+        return tr
+
+    def time_extractor(self, tp, ts):
+        nxyz= num.size(tp[self.stations[0]])
+        tp_mod=num.zeros([nxyz,self.nstation]) #optimize with pointers to tp[sta] and ts[sta]
+        ts_mod=num.zeros([nxyz,self.nstation])
+        for i,sta in enumerate(self.stations):
+            tp_mod[:,i]=tp[sta]
+            ts_mod[:,i]=ts[sta]
+        return (tp_mod, ts_mod)
+
+    def cfunc_erg(self, ergz=True):
+        if ergz:
+           self.obs_dataV=(self.ztr**2)
+        else:
+           self.obs_dataV=(self.ztr**2)
+           self.obs_dataH=(self.xtr**2)*(self.ytr**2)
+
+    def cfunc_pcafull(self, epsilon=0.001):
+        obs_dataH=num.zeros([self.nstation,self.ns]); obs_dataV=num.zeros([self.nstation,self.ns])
+        obs_dataH1=hilbert(self.xtr); obs_dataH2=hilbert(self.ytr); obs_dataH3=hilbert(self.ztr)
+        obs_dataH1C=num.conjugate(obs_dataH1); obs_dataH2C=num.conjugate(obs_dataH2); obs_dataH3C=num.conjugate(obs_dataH3)
+        xx=obs_dataH1*obs_dataH1C; xy=obs_dataH1*obs_dataH2C; xz=obs_dataH1*obs_dataH3C
+        yx=obs_dataH2*obs_dataH1C; yy=obs_dataH2*obs_dataH2C; yz=obs_dataH2*obs_dataH2C
+        zx=obs_dataH3*obs_dataH1C; zy=obs_dataH3*obs_dataH2C; zz=obs_dataH3*obs_dataH3C
+        for i in range(self.nstation):
+            for j in range(self.ns):
+                cov3d=num.array([[xx[i,j], xy[i,j], xz[i,j]],[yx[i,j], yy[i,j], yz[i,j]],[zx[i,j],zy[i,j],zz[i,j]]])
+                cov2d=num.array([[xx[i,j], xy[i,j]],[yx[i,j], yy[i,j]]])
+                U2d, s2d, V2d = num.linalg.svd(cov2d, full_matrices=True)
+                U3d, s3d, V3d = num.linalg.svd(cov3d, full_matrices=True)
+                obs_dataV[i,j]=(s3d[0]**2)
+                obs_dataH[i,j]=(s2d[0]**2)
+            obs_dataH[i,:]=(obs_dataH[i,:]/num.max(obs_dataH[i,:]))+epsilon
+            obs_dataV[i,:]=(obs_dataV[i,:]/num.max(obs_dataV[i,:]))
+        self.obs_dataH=obs_dataH
+        self.obs_dataV=obs_dataV
+
+    def cfunc_pca(self, epsilon=0.001):
+        obs_dataH=num.zeros([self.nstation,self.ns])
+        obs_dataH1=hilbert(self.xtr)
+        obs_dataH2=hilbert(self.ytr)
+        obs_dataH1C=num.conjugate(obs_dataH1)
+        obs_dataH2C=num.conjugate(obs_dataH2)
+        xx=obs_dataH1*obs_dataH1C; xy=obs_dataH1*obs_dataH2C
+        yx=obs_dataH2*obs_dataH1C; yy=obs_dataH2*obs_dataH2C
+        for i in range(self.nstation):
+            for j in range(self.ns):
+                cov=num.array([[xx[i,j], xy[i,j]],[yx[i,j], yy[i,j]]])
+                U, s, V = num.linalg.svd(cov, full_matrices=True)
+                obs_dataH[i,j]=(s[0]**2)
+            obs_dataH[i,:]=(obs_dataH[i,:]/num.max(obs_dataH[i,:]))+epsilon
+        self.obs_dataH=obs_dataH
+
+    def loc_stalta(self, nshort_p, nshort_s, slrat, thres=2):
+        tshort_p=nshort_p*self.deltat; tshort_s=nshort_s*self.deltat
+        tlong_p=tshort_p*slrat; tlong_s=tshort_s*slrat
+        ks_p=self.deltat/tshort_p; kl_p=self.deltat/tlong_p;
+        ks_s=self.deltat/tshort_s; kl_s=self.deltat/tlong_s;
+        obs_dataP=LOC_STALTA.recursive_stalta(tshort_p, tlong_p, self.deltat, self.obs_dataV, kl_p, ks_p, thres)
+        obs_dataS=LOC_STALTA.recursive_stalta(tshort_s, tlong_s, self.deltat, self.obs_dataH, kl_s, ks_s, thres)
+        return obs_dataP, obs_dataS
+
+    def det_stalta(self, nshort_p, nshort_s, slrat, staltap0, staltas0, thres=0.):
+        tshort_p=nshort_p*self.deltat; tshort_s=nshort_s*self.deltat
+        tlong_p=tshort_p*slrat; tlong_s=tshort_s*slrat
+        obs_dataP=num.zeros([self.nstation,self.ns])
+        obs_dataS=num.zeros([self.nstation,self.ns])
+        for i in range(self.nstation):
+            obs_dataP[i,:],stltp0=DET_STALTA.recursive_stalta(staltap0[i][0], staltap0[i][1], tshort_p, tlong_p, self.deltat, self.obs_dataV[i,:], thres)
+            obs_dataS[i,:],stlts0=DET_STALTA.recursive_stalta(staltas0[i][0], staltas0[i][1], tshort_s, tlong_s, self.deltat, self.obs_dataH[i,:], thres)
+            staltap0[i][0]=stltp0[0]; staltap0[i][1]=stltp0[1]
+            staltas0[i][0]=stlts0[0]; staltas0[i][1]=stlts0[1]
+        return obs_dataP, obs_dataS, staltap0, staltas0
