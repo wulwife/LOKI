ce456e3da8ec443577d6beb266dce7d965139410
Francesco <francesco.grigoli@sed.ethz.ch>
2018-11-15 08:08:24 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# to call loki you need
+# to add the path of the src_python folder to the PYTHONPATH
+# export PYTHONPATH=${PYTHONPATH}:/Users/francesco/Desktop/LOKI_GIT/LOKI/src_python
+
+import loki
+import numpy as num
+
+db_path='./Test_dataset/Traveltimes'
+data_path='./Test_dataset/Data'
+output_path='./Test_dataset/output'
+hdr_filename='header.hdr'
+inputs={}
+inputs['tshortp_min']=0.1
+inputs['tshortp_max']=0.1
+inputs['tshorts_min']=0.15
+inputs['tshorts_max']=0.15
+inputs['slrat']=2
+inputs['npr']=2
+inputs['ntrial']=1
+inputs['derivative']=True
+inputs['vfunc']='erg'
+inputs['hfunc']='pca'
+inputs['model']='layer'
+inputs['epsilon']=0.001
+precision='single'
+comp=['E','N','Z']
+extension='*'
+#test
+
+l1=loki.Loki(data_path, output_path, db_path, hdr_filename, mode='locator')
+l1.location(extension, comp, precision, **inputs)
